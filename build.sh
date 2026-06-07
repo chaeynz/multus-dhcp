f61d03186e31c14a574930450c2b8eeaa68e8d42
chaeynz <chaeynz4msg@gmail.com>
2026-06-07 15:21:58 +0200

    fix: build script didnt expect digest pin

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALPINE_TAG=$(grep '^FROM alpine:' Dockerfile | head -1 | cut -d':' -f2 | cut -d' ' -f1)
+ALPINE_TAG=$(grep '^FROM alpine:' Dockerfile | head -1 | cut -d':' -f2 | cut -d' ' -f1 | cut -d'@' -f1)
 
 source cni_plugins_version.env
 
